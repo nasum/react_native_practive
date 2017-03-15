1f503470613af6429b27b0181d6ce90356f58f69
nasum <tomato.wonder.life@gmail.com>
2017-03-15 23:34:20 +0900

    add scroll view

--- index.android.js
+++ index.android.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, Image, View, Text } from 'react-native';
+import { AppRegistry, Image, View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
 
 class Greeting extends Component {
   render() {
@@ -28,10 +28,52 @@ class Blink extends Component {
   render() {
     let display = this.state.showText ? this.props.text : ' ';
     return (
-      <Text>{display}</Text>
+      <Text style={styles.red}>{display}</Text>
     )
   }
 }
+
+class TextInputComponent extends Component {
+  constructor(props){
+    super(props);
+    this.state = { text: "" };
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="Type here"
+          onChangeText={(text) => this.setState({text})}
+        >
+        </TextInput>
+        <Text>
+          {this.state.text}
+        </Text>
+      </View>
+    )
+  }
+}
+
+class ScrollViewComponent extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Text style={{fontSize:96}}>If you like</Text>
+        <Text style={{fontSize:96}}>Scrolling down</Text>
+        <Text style={{fontSize:96}}>What's the best</Text>
+        <Text style={{fontSize:96}}>Framework around?</Text>
+        <Text style={{fontSize:80}}>React Native</Text>
+      </ScrollView>
+    );
+  }
+}
+
 
 class react_native_practice extends Component {
   render() {
@@ -43,9 +85,18 @@ class react_native_practice extends Component {
         <Image source={pic} style={{width: 193, height: 110}}/>
         <Greeting name='Rexxar' />
         <Blink text="hogehoge" />
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <TextInputComponent />
+        <ScrollViewComponent />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  red: {
+    color: 'red'
+  }
+})
 
 AppRegistry.registerComponent('react_native_practice', () => react_native_practice);
